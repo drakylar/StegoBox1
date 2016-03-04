2b5dd6733503d232392dbdf12aa836cfb113f7ff
iljashaposhnikov <iljashaposhnikov@gmail.com>
2016-03-04 22:32:41 +0300

    New project

--- filters.py
+++ filters.py
@@ -1,2 +1,28 @@
+from PIL import Image, ImageTk
+def rotate90(image):
+    return image.transpose(Image.ROTATE_90)
 
-def FirstFilter():
+def rotate180(image):
+    return image.transpose(Image.ROTATE_180)
+
+def sepia(image):
+    pixels = image.convert('RGB')
+    width, height = image.size
+    r, g, b = image.getpixel((1, 1))
+    for x in range(width):
+        for y in range(height):
+            r, g, b = image.getpixel((x, y))
+            c = (r+g+b)//3
+            image.putpixel( (x,y), (c,c,c))
+    return image
+
+def whitePixels(image):
+    pixels = image.convert('RGB')
+    width, height = image.size
+    r, g, b = image.getpixel((1, 1))
+    for x in range(width):
+        for y in range(height):
+            r, g, b = image.getpixel((x, y))
+            if not r==g==b:
+                image.putpixel( (x,y), (0,0,0))
+    return image
--- steganography.py
+++ steganography.py
@@ -1,17 +1,35 @@
+from copy import deepcopy
+
 from PIL import Image, ImageTk
 import tkinter as tk
-import language
+import language,filters
 from tkinter import filedialog, SE, messagebox
 def main():
-    global FiltersSet
-    FiltersSet = {}
+    global ChangeLog,FiltersSet
+    FiltersSet = []
+    ######rotate90######
+    FiltersSet.append(0)
+    ######rotate180######
+    FiltersSet.append(0)
+    ######sepia######
+    FiltersSet.append(0)
+    ######white_pix######
+    FiltersSet.append(0)
 
-    global ChangeLog
 
     ChangeLog = []
 
     languageChoose()
     createWindows()
+
+    """
+    0 - rotate90
+    """
+
+
+
+
+
     imageWindow.mainloop()
 def createWindows():
     """
@@ -72,16 +90,22 @@ def imageChoose():
         showOriginalImage()
 
 def showOriginalImage():
-    global ImageLabel,originalImage,changedImage
+    global ImageLabel,originalImage,changedImage,ChangeLog
     if 'ImageLabel' in globals(): ImageLabel.destroy()
     originalImage = Image.open(openedPicture)
-    photo = ImageTk.PhotoImage(originalImage)
-    ImageLabel = tk.Label(image=photo)
-    ImageLabel.image = photo
-    ImageLabel.pack()
-    changePreview(originalImage)
+    AddPictureToChangelog('original',originalImage)
+    showImage(originalImage)
     imageWindow.mainloop()
 
+
+def showImage(image):
+    global ImageLabel
+    photo = ImageTk.PhotoImage(image)
+    if 'ImageLabel' in globals(): ImageLabel.destroy()
+    ImageLabel = tk.Label(image=photo)
+    ImageLabel.image = photo
+    print('preview')
+    ImageLabel.pack()
 
 def preview():
     global previeWindow
@@ -113,44 +137,189 @@ def filterWindowFunction():
     Window with filter options
     """
 
-    global firstFilter
+    global firstFilter,originalImage,FiltersSet,previeWindow
 
     ##################################################################
     def changeFirstFilter():
+        global FiltersSet
         """
         FIRST Filtet Oprions
         """
-        try:
-            z =(FiltersSet.get('FirstFilter')+1)%2
-            FiltersSet.update({'FirstFilter':z})
-        except:
-            FiltersSet.update({'FirstFilter':1})
+        print(FiltersSet[0])
+        if FiltersSet[0]==0:
+                FiltersSet[0]=1
+                image = filters.rotate90(deepcopy(ChangeLog[len(ChangeLog)-1][1]))
+                AddPictureToChangelog('rotate90',image)
+                showImage(image)
+                changePreview(image)
+                previeWindow.mainloop()
+                imageWindow.mainloop()
+        elif FiltersSet[0]==1:
+                FiltersSet[0]=0
+                image = RemovePictureFromChangelog('rotate90')
+                showImage(image)
+                changePreview(image)
+                previeWindow.mainloop()
+                imageWindow.mainloop()
 
     ##################################################################
+    ##################################################################
+    def changeSecondFilter():
+        global FiltersSet
+        """
+        second Filtet Oprions
+        """
+        if FiltersSet[1]==0:
+                FiltersSet[1]=1
+                image = filters.rotate180(deepcopy(ChangeLog[len(ChangeLog)-1][1]))
+                AddPictureToChangelog('rotate180',image)
+                showImage(image)
+                changePreview(image)
+                previeWindow.mainloop()
+                imageWindow.mainloop()
+        elif FiltersSet[1]==1:
+                FiltersSet[1]=0
+                image = RemovePictureFromChangelog('rotate180')
+                showImage(image)
+                changePreview(image)
+                previeWindow.mainloop()
+                imageWindow.mainloop()
+    ##################################################################
+    ##################################################################
+    def changeThirdFilter():
+        global FiltersSet
+        """
+        third Filter Oprions
+        """
+        if FiltersSet[2]==0:
+                FiltersSet[2]=1
+                image = filters.sepia(deepcopy(ChangeLog[len(ChangeLog)-1][1]))
+                AddPictureToChangelog('sepia',image)
+                showImage(image)
+                changePreview(image)
+                previeWindow.mainloop()
+                imageWindow.mainloop()
+        elif FiltersSet[2]==1:
+                FiltersSet[2]=0
+                image = RemovePictureFromChangelog('sepia')
+                showImage(image)
+                changePreview(image)
+                previeWindow.mainloop()
+                imageWindow.mainloop()
+    ##################################################################
+    ##################################################################
+
+    def changeFourthFilter():
+        global FiltersSet
+        """
+        third Filter Options
+        """
+        if FiltersSet[3]==0:
+                FiltersSet[3]=1
+                image = filters.whitePixels(deepcopy(ChangeLog[len(ChangeLog)-1][1]))
+                AddPictureToChangelog('white_pix',image)
+                showImage(image)
+                changePreview(image)
+                previeWindow.mainloop()
+                imageWindow.mainloop()
+        elif FiltersSet[3]==1:
+                FiltersSet[3]=0
+                image = RemovePictureFromChangelog('white_pix')
+                showImage(image)
+                changePreview(image)
+                previeWindow.mainloop()
+                imageWindow.mainloop()
+    ##################################################################
+
 
 
 
     filterWindow = tk.Toplevel(width=500)
     filterWindow.geometry('210x100+1000+0')
     filterWindow.title(lang[18])
+
+    ###################################################
+    #                 first filter                    #
+    ###################################################
+
+
     firstFilter = tk.Checkbutton(filterWindow,
-                                     text = lang[16],
+                                     text = lang[15],
                                      onvalue = 1,
                                      offvalue = 0,
                                      command = changeFirstFilter
                                  )
     firstFilter.pack()
 
+    ###################################################
+    #                 second filter                   #
+    ###################################################
+
+
+    secondFilter = tk.Checkbutton(filterWindow,
+                                     text = lang[16],
+                                     onvalue = 1,
+                                     offvalue = 0,
+                                     command = changeSecondFilter
+                                 )
+    secondFilter.pack()
+
+    ###################################################
+    #                 third filter                    #
+    ###################################################
+
+
+    thirdFilter = tk.Checkbutton(filterWindow,
+                                     text = lang[19],
+                                     onvalue = 1,
+                                     offvalue = 0,
+                                     command = changeThirdFilter
+                                 )
+    thirdFilter.pack()
+
+    ###################################################
+    #                 fourth filter                   #
+    ###################################################
+
+
+    fourthFilter = tk.Checkbutton(filterWindow,
+                                     text = lang[20],
+                                     onvalue = 1,
+                                     offvalue = 0,
+                                     command = changeFourthFilter
+                                 )
+    fourthFilter.pack()
+
+
+
 def AddPictureToChangelog(filter,image):
     global ChangeLog
     ChangeLog.append([filter,image])
+    print('addchange',ChangeLog)
 
-def RemovePictureFromChangelog():
+def RemovePictureFromChangelog(filter):
+    global ChangeLog,originalImage
+    cross = []
+    print('removechange',filter,ChangeLog )
+    for x in ChangeLog:
+        if x[0]==filter: cross=x
+    i = ChangeLog.index(cross)
+    ChangeLog.remove(cross)
+    print(i)
+    print('removechange',filter,ChangeLog )
+    if i == 0: startimage = originalImage
+    elif i > 0: startimage = ChangeLog[i-1][1]
+    print(i)
+    while i<len(ChangeLog):
+        if ChangeLog[i][0]=='rotate90': startimage = filters.rotate90(startimage)
+        elif ChangeLog[i][0]=='rotate180': startimage = filters.rotate180(startimage)
+        elif ChangeLog[i][0]=='sepia': startimage = filters.sepia(startimage)
+        elif ChangeLog[i][0]=='white_pix': startimage = filters.whitePixels(startimage)
+        i+=1
+    return startimage
 
 
 
-def ChangePicture(text_filter):
-    global ChangeLog
 
 
 main()
